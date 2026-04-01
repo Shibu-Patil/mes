04ea6cdebab5bb9fe6c5249b7a0cc6efa55da69f
Shibu-Patil <patilshibu@gmail.com>
2026-04-01 18:44:10 +0530

    fixed size

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 10 * 1024 * 1024 } // 10MB
+  limits: { fileSize: 100 * 1024 * 1024 } // 10MB
 });
 
 export default upload;
